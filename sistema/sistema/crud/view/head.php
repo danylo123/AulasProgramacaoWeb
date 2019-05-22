45f6fd241df4fea1ec39a328bc097ace83c4b828
danylo123 <danyloalyson1@gmail.com>
2019-05-22 20:06:18 -0300

    Importação do bootstrap e configuração login 22/05
    Importação do bootstrap e configuração do login 22/05

---
+++ sistema/sistema/crud/view/head.php
@@ -0,0 +1,7 @@
+
+<meta charset="utf-8">
+<title>Sistema CRUD</title>
+<!-- Bootstrap CSS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
